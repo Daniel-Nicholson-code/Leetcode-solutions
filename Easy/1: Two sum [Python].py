2c0f8bdd327661e29f6814a353329dbe19f03a56
Daniel Nicholson <danielnicholson239@gmail.com>
2025-08-16 22:42:33 +0100

    Update 1: Two sum [Python].py

--- Easy/1: Two sum [Python].py
+++ Easy/1: Two sum [Python].py
@@ -1,2 +1,19 @@
-# Copyright 2025 Daniel Nicholson
-# If using my code please credit me
+# Complexity: O(n)
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        
+        index = [0, 0]
+        hashmap = {}
+
+        for i in range(len(nums)):
+
+            value = nums[i]
+
+            if target - value in hashmap:
+                index = [i, hashmap[target - value]]
+                break
+            
+            hashmap[value] = i
+                
+        return index
